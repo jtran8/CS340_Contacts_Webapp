a32a72073910c07ae12a49dfeea637bdf092d201
George Hill <hillge@oregonstate.edu>
2020-02-23 17:16:02 -0500

    Created queriesForBackend.sql.

---
+++ sql/queriesForBackend.sql
@@ -0,0 +1,16 @@
+-- Throughout this file, double quotes are used to set off variables that will
+-- be provided by the backend programming language.
+
+-- Queries needed by /home.html:
+
+-- Queries needed by /myAccount.html:
+
+-- Queries needed by /contacts.html:
+
+-- Queries needed by /editContact.html:
+
+-- Queries needed by /viewInteractions.html:
+
+-- Queries needed by /addModeToInteraction.html:
+
+-- Queries neeed by /modes.html:

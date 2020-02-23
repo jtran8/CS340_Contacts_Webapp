154bd98d14a35eb5ff70135d6b12b14992d79a10
George Hill <hillge@oregonstate.edu>
2020-02-23 18:18:13 -0500

    Added queries for /myAccount.html.

--- sql/queriesForBackend.sql
+++ sql/queriesForBackend.sql
@@ -1,6 +1,8 @@
 -- Throughout this file, single quotes are used to set off variables that will
 -- be provided by the backend programming language.
 
+
+
 -- Queries needed by /home.html:
 
 -- -- Query to generate list of users (and user IDs):
@@ -12,18 +14,40 @@ SELECT userId, lastName, firstName FROM users;
 INSERT INTO users (lastName, firstName, phone, email) VALUES
 ('LastNameFromUserInput', 'FirstNameFromUserInput', 'PhoneNumberFromUserInput', 'EmailAddressFromUserInput');
 
+
+
 -- Queries needed by /myAccount.html:
 
 -- -- Query to get user's name:
 
 SELECT lastName, firstName FROM users WHERE userID='UserIdFromURL';
 
+-- -- Query to get current information for current user:
+
+SELECT lastName, firstName, phone, email FROM users WHERE userID='UserIdFromURL';
+
+-- -- Query to update information for current user:
+
+UPDATE users
+SET lastName='LastNameFromUserInput', firstName='FirstNameFromUserInput', phone='PhoneFromUserInput', email='EmailFromUserInput'
+WHERE userId='UserIdFromURL';
+
+
+
 -- Queries needed by /contacts.html:
+
+
 
 -- Queries needed by /editContact.html:
 
+
+
 -- Queries needed by /viewInteractions.html:
 
+
+
 -- Queries needed by /addModeToInteraction.html:
+
+
 
 -- Queries neeed by /modes.html:

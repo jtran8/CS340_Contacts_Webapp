bce0cdcf03d653802921e26a24cbe1b742484882
George Hill <hillge@oregonstate.edu>
2020-02-23 14:26:03 -0500

    Added sample users data.

--- SQL/insertSampleData.sql
+++ SQL/insertSampleData.sql
@@ -2,3 +2,7 @@
 
 -- Its use of foreign keys assumes that it is being run on an empty database.
 
+INSERT INTO users (lastName, firstName, phone, email) VALUES
+('Macintosh', 'Morgan', '555-555-1001', 'morgan@example.com'),
+('Stone', 'Skyler', '555-555-1002', 'skyler@example.com'),
+('Adams', 'Armani', '555-555-1003', 'armani@example.com');

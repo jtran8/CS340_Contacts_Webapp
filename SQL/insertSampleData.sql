1a8df9beb8d59a9b46c8faab5076b42845e1bd11
George Hill <hillge@oregonstate.edu>
2020-02-23 15:38:25 -0500

    Changed base index from 5 to 1.

--- SQL/insertSampleData.sql
+++ SQL/insertSampleData.sql
@@ -8,15 +8,15 @@ INSERT INTO users (lastName, firstName, phone, email) VALUES
 ('Adams', 'Armani', '555-555-1003', 'armani@example.com');
 
 INSERT INTO contacts (userId, lastName, firstName, phone, email, notes, emergencyContactId) VALUES
-(5, 'Jung', 'Jordan', '617-555-1004', 'jordan@example.com', 'Birthday is Jan. 1.', NULL),
-(5, 'Smith', 'Sydney', '617-555-1005', 'sydney@example.com', 'Has a dog.', 5),
-(5, 'Rogers', 'Robin', '617-555-1006', 'robin@example.com', 'Software engineer.', NULL);
+(1, 'Jung', 'Jordan', '617-555-1004', 'jordan@example.com', 'Birthday is Jan. 1.', NULL),
+(1, 'Smith', 'Sydney', '617-555-1005', 'sydney@example.com', 'Has a dog.', 1),
+(1, 'Rogers', 'Robin', '617-555-1006', 'robin@example.com', 'Software engineer.', NULL);
 
 INSERT INTO interactions (contactId, `start`) VALUES
-(5, '2000-01-01 09:00:00'),
-(5, '2000-01-02 10:00:00'),
-(5, '2000-01-03 11:00:00'),
-(5, '2000-01-13 12:00:00');
+(1, '2000-01-01 09:00:00'),
+(1, '2000-01-02 10:00:00'),
+(1, '2000-01-03 11:00:00'),
+(1, '2000-01-13 12:00:00');
 
 INSERT INTO communicationModes (`type`) VALUES
 ('SMS Message'),
@@ -24,8 +24,8 @@ INSERT INTO communicationModes (`type`) VALUES
 ('Meeting');
 
 INSERT INTO interactionDetails (interactId, comId, details) VALUES
-(5, 5, 'Made plans to meet up over coffee or lunch.'),
-(6, 6, 'Chose a spot and time for lunch.'),
-(7, 7, 'Met up for lunch at Queen Margherita Pizza; he forgot his wallet so I covered.'),
-(8, 5, 'Reminded him he owes me for lunch.'),
-(8, 6, 'He called me apologize for taking so long to repay him.');
+(1, 1, 'Made plans to meet up over coffee or lunch.'),
+(2, 2, 'Chose a spot and time for lunch.'),
+(3, 3, 'Met up for lunch at Queen Margherita Pizza; he forgot his wallet so I covered.'),
+(4, 1, 'Reminded him he owes me for lunch.'),
+(4, 2, 'He called me apologize for taking so long to repay him.');
